5ee7c77a4e6d2ba8d2c87e2fee8bb40ca4a1447b
lepilov_alexander <temnoles666@gmail.com>
2017-10-14 19:10:04 +0300

    add plus()

---
+++ 1.js
@@ -0,0 +1,10 @@
+function plus() {
+	var num1, num2;
+	num1 = document.getElementById("n1").value;
+	num1 = parseInt(num1);
+
+	num2 = document.getElementById("n2").value;
+	num2 = parseInt(num2);
+
+	document.getElementById('out').innerHTML = result;
+}
